3eb34d9838e528e55dc15bb59cbe4e87b2cd9612
FrostE2220107 <148461673+FrostE2220107@users.noreply.github.com>
2023-10-19 14:37:27 +0100

    Upload 1
    My task from week 2

---
+++ WS_2_task.php
@@ -0,0 +1,8 @@
+<?php
+// https://mi-linux.wlv.ac.uk/~2220107/Y2/WS_2_task.php is the link to access
+	$my_year = 2000;
+	while ($my_year != 2003) {
+		$my_year = rand(1970, 2010);
+		echo "<br>" . $my_year;}
+		echo ": The year when I was born!"
+?>
